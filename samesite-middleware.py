846270962175dbec93a2c357c732682cf1126c0c
rasoolsomji <rsomji@gmail.com>
2019-11-26 14:16:25 +0000

    Update samesite-middleware.py

--- samesite-middleware.py
+++ samesite-middleware.py
@@ -10,7 +10,7 @@ def cookies_samesite(get_response):
     def middleware(request):
 
         response = get_response(request)
-		for cookie in [settings.CSRF_COOKIE_NAME, settings.SESSION_COOKIE_NAME]:
+	for cookie in [settings.CSRF_COOKIE_NAME, settings.SESSION_COOKIE_NAME]:
             if cookie in response.cookies:
                 response.cookies[cookie]['samesite'] = 'lax'
 
